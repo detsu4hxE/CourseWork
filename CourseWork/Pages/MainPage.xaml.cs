a978ae1e4ab09e457c67aca4df0999729c867142
detsu4hxE <rybalkomihail744@gmail.com>
2023-05-30 13:05:19 +0300

    Создание страницы для проверки кода

---
+++ CourseWork/Pages/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+
+namespace CourseWork.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+            ICodeCompiler icc = codeProvider.CreateCompiler();
+            string Output = "Out.exe";
+            Button ButtonObject = (Button)sender;
+
+            testBox.Text = "";
+            System.CodeDom.Compiler.CompilerParameters parameters = new CompilerParameters();
+            //Make sure we generate an EXE, not a DLL
+            parameters.GenerateExecutable = true;
+            parameters.OutputAssembly = Output;
+            CompilerResults results = icc.CompileAssemblyFromSource(parameters, testBox.Text);
+
+            if (results.Errors.Count > 0)
+            {
+                foreach (CompilerError CompErr in results.Errors)
+                {
+                    testBox.Text = testBox.Text +
+                                "Line number " + CompErr.Line +
+                                ", Error Number: " + CompErr.ErrorNumber +
+                                ", '" + CompErr.ErrorText + ";" +
+                                Environment.NewLine + Environment.NewLine;
+                }
+            }
+            else
+            {
+                //Successful Compile
+                testBox.Text = "Success!";
+            }
+        }
+    }
+}
--- CourseWork/Windows/MainWindow.xaml.cs
+++ CourseWork/Windows/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace CourseWork.Windows
             InitializeComponent();
             currentUserId = user_id;
             Update(user_id);
-            FrameMain.Navigate(new Pages.AnswersPage(currentUserId));
+            FrameMain.Navigate(new Pages.MainPage());
         }
         private void Update(int user_id)
         {
